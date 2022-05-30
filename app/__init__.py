240483b9ced281f72afe03dc67b28e19093ead0f
jeanpiffaut <piffaut.jean@gmail.com>
2022-05-29 23:26:07 -0400

    add init app

---
+++ app/__init__.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+
+from app.common.error_handling import ObjectNotFound, AppErrorBaseClass
+from app.db import db
+from app.films.api_v1_0.resources import films_v1_0_bp
+from .ext import ma, migrate
+
+
+def create_app(setting_module):
+    app = Flask(__name__)
+    app.config.from_object(setting_module)
+
+    db.init_app(app)
+    ma.init_app(app)
+    migrate.init_app(app, db)
+
+    Api(app, catch_all_404s=True)
+
+    app.url_map.strict_slashes = False
+
+    app.register_blueprint(films_v1_0_bp)
+
+    register_error_hablder(app)
+
+    return app
+
+
+def register_error_hablder(app):
+    @app.errorhandler(Exception)
+    def handle_exeption_error(e):
+        return jsonify({'msj': 'Internal server error'}), 500
+
+    @app.errorhandler(405)
+    def handle_405_error(e):
+        return jsonify({'msj': 'Method not allowed'}), 405
+
+    @app.errorhandler(403)
+    def handle_403_error(e):
+        return jsonify({'msj': 'Forbidden error'}), 403
+
+    @app.errorhandler(404)
+    def handle_404_error(e):
+        return jsonify({'msj': 'Not found error'}), 404
+
+    @app.errorhandler(AppErrorBaseClass)
+    def handle_app_base_error(e):
+        return jsonify({'msj': str(e)}), 500
+
+    @app.errorhandler(ObjectNotFound)
+    def handle_objetc_not_found_error(e):
+        return jsonify({'msj': str(e)}), 404
+
